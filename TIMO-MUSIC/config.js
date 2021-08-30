787a3fb0ae925efdd9b6246420a0b7b3e4f096c0
Lawinlatif5 <89773341+Lawinlatif5@users.noreply.github.com>
2021-08-30 11:38:35 +0300

    Update config.js

--- TIMO-MUSIC/config.js
+++ TIMO-MUSIC/config.js
@@ -1,5 +1,5 @@
 const config = {
-    token: 'ODM1MDQ1MjcxNTE5NDk0MTQ0.YIJugw.oontXsjrZl9xMVdnC56lwTLgEis',
-    prefix: '+'
+    token: 'ODgxNDkyMzgwODc0ODU0NDAw.YStnwA.s2JC_qz6Mz3k6UNsZjlpQIcCp4g',
+    prefix: '!'
 }
 module.exports = config;
